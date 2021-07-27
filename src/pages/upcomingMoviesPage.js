2bc8acf86625c483f701578402c1de589d53c695
gavin-soady <gsoady19@gmail.com>
2021-07-27 06:41:58 +0100

    Fix upcoming movies page

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -2,22 +2,35 @@ import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { withRouter } from "react-router-dom";
+import { useQuery } from 'react-query'
+import Spinner from '../components/spinner'
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const UpcomingMoviesPage = (props) => {
-  const [movies, setMovies] = useState([]);
- 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const {  data, error, isLoading, isError }  = useQuery('discover', getUpcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+  //const addToFavorites = (movieId) => true 
 
   return (
     <PageTemplate
-      title="Upcoming Movies"
+      title="Discover Movies"
       movies={movies}
-    />
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
+    />    
   );
 };
 
